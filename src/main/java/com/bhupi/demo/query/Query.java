0c624e1f92ba5b0f6ac5b06000c92a69c89cc165
Singh <bhupendra.e.singh@capgemini.com>
2024-03-11 13:45:46 +0200

    GraphQL example with input arguments

--- src/main/java/com/bhupi/demo/query/Query.java
+++ src/main/java/com/bhupi/demo/query/Query.java
@@ -1,6 +1,8 @@
 package com.bhupi.demo.query;
 
 
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.Arguments;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -15,5 +17,10 @@ public class Query  {
     @QueryMapping
     public String secondQuery() {
         return "Second Query";
+    }
+
+    @QueryMapping
+    public String fullName(@Argument String firstName, @Argument String lastName) {
+        return firstName + " " + lastName;
     }
 }
