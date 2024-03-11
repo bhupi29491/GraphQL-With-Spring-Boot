31adee9fe9a8097ae372daacc38d0b10003c32ea
Singh <bhupendra.e.singh@capgemini.com>
2024-03-11 13:14:32 +0200

    Initial GraphQL POC with query returning a String data

---
+++ src/main/java/com/bhupi/demo/GraphQlWithSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.bhupi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class GraphQlWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bhupi/demo/query/Query.java
@@ -0,0 +1,19 @@
+package com.bhupi.demo.query;
+
+
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Query  {
+
+    @QueryMapping
+    public String firstQuery() {
+        return "First Query";
+    }
+
+    @QueryMapping
+    public String secondQuery() {
+        return "Second Query";
+    }
+}
